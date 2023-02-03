4b26dcc229c45e53745a343b1398fc47ca2510ba
Jimmy <jimmystry@gmail.com>
2023-02-02 20:26:34 -0500

    Initial test which generates x random points around start, adds to tree

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pygame
+from rrt import RRT
 
 pygame.init()
 
@@ -8,6 +9,7 @@ screen = pygame.display.set_mode(size)
 black = 255, 255, 255
 
 running = True
+test_tree = RRT((200, 200), (200, 200), 20, 50)
 
 while running:
     for event in pygame.event.get():
@@ -15,7 +17,11 @@ while running:
             running = False
     
     screen.fill(black)
-    pygame.draw.circle(screen, (255, 255, 255), (200, 200), 2)
+    pygame.draw.circle(screen, (0, 0, 0), (200, 200), 5);
+
+    for node in test_tree:
+        pygame.draw.circle(screen, (0, 0, 255), (node[1], node[2]), 2)
+
     pygame.display.flip()
 
 pygame.quit()
--- rrt.py
+++ rrt.py
@@ -17,6 +17,29 @@ def samplePointsAround(x, y, r, numPts):
 
     return pts
 
+"""
+    Rapidly-exploring Random Tree for parafoil pathfinding
+    This algorithm will begin at the start coordinate, and sample random points around the current position and goal position.
+    It will then determine which of these points are viable given the turn radius and maximum distance travellable.
+    Once viable points are found, it will connect them to the current point, and for each of these new points repeat this process until we reach the goal.
+    startCoord: starting location, tuple of (x, y)
+    goalCoord: goal location, tuple of (x, y)
+    maxIterations: maximum number of iterations to run the algorithm
+    maxDistTravellable: maximum distance that can be travelled in one iteration
+"""
+def RRT(startCoord, goalCoord, maxIterations, maxDistTravellable):
+    startX, startY = startCoord
+    startPoint = (None, startX, startY) # all points follow this structure of (parentPoint, x, y)
+    randomPoints = samplePointsAround(startX, startY, maxDistTravellable, 20)
 
-test = samplePointsAround(200, 200, 100, 10);
-print(test)
+    # TODO: determine which of these points are viable
+
+    tree = []
+
+    for point in randomPoints:
+        x, y = point
+        newNode = (startPoint, x, y)
+        tree.append(newNode)
+
+    # TODO: finish
+    return tree
